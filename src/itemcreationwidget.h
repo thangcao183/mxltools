04c5b05d56b32c027760404f1e1ff985e8ec02d9
thangcao183 <thangcao183@gmail.com>
2025-10-16 13:02:40 +0700

    fix: match member name to implementation (_itemCombo) in ItemCreationWidget

---
+++ src/itemcreationwidget.h
@@ -0,0 +1,49 @@
+#ifndef ItemCREATIONWIDGET_H
+#define ItemCREATIONWIDGET_H
+
+#include <QDialog>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QLabel>
+#include <QComboBox>
+
+class ItemInfo;
+
+class ItemCreationWidget : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ItemCreationWidget(QWidget *parent = nullptr);
+    ~ItemCreationWidget();
+
+    ItemInfo* getCreatedOrb() const { return _createdOrb; }
+    int copies() const { return _copies; }
+    int requestedRow() const { return _targetRow; }
+    int requestedColumn() const { return _targetCol; }
+
+    void setItemPosition(int row, int column);
+
+private slots:
+    void onCreateClicked();
+    void onCancelClicked();
+
+private:
+    void setupUI();
+    ItemInfo* createItemItem();
+
+    QSpinBox *_rowSpin;
+    QSpinBox *_colSpin;
+    QSpinBox *_copiesSpin;
+    QComboBox *_itemCombo;
+    QLabel *_previewLabel;
+    QPushButton *_createButton;
+    QPushButton *_cancelButton;
+
+    ItemInfo *_createdOrb;
+    int _targetRow;
+    int _targetCol;
+    int _copies;
+};
+
+#endif // ItemCREATIONWIDGET_H
